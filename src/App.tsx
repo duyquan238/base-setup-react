05102aab359a8839b0b20afb8ee677b4c8d0a48d
nguyeq1 <Quan.NguyenDuy@emeal.nttdata.com>
2025-05-29 13:36:28 +0700

    Setup and initial project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-100 p-6 rounded-lg flex justify-center items-center min-h-screen flex-col">
+        <h1 className="text-2xl font-bold mb-4">Welcome</h1>
+        <Button type="primary">Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
